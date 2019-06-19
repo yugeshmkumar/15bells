11446c05bd34ee5b74cac19c0900188c754e5f27
Harish Rajput <doublehrajput@gmail.com>
2019-06-19 11:46:25 +0530

    supply form

--- backend/views/addproperty-onepage-form/_form.php
+++ backend/views/addproperty-onepage-form/_form.php
@@ -25,8 +25,79 @@ label{
     font-family: inherit;
     font-size: 12px;
 }
+#infowindow-content .title {
+        font-weight: bold;
+      }
+
+      #infowindow-content {
+        display: none;
+      }
+
+      #map #infowindow-content {
+        display: inline;
+      }
+
+      .pac-card {
+        margin: 10px 10px 0 0;
+        border-radius: 2px 0 0 2px;
+        box-sizing: border-box;
+        -moz-box-sizing: border-box;
+        outline: none;
+        box-shadow: 0 2px 6px rgba(0, 0, 0, 0.3);
+        background-color: #fff;
+        font-family: Roboto;
+      }
+
+      #pac-container {
+        padding-bottom: 12px;
+        margin-right: 12px;
+      }
+
+      .pac-controls {
+        display: inline-block;
+        padding: 5px 11px;
+      }
+
+      .pac-controls label {
+        font-family: Roboto;
+        font-size: 13px;
+        font-weight: 300;
+      }
+
+      #pac-input {
+        background-color: #fff;
+        font-family: Roboto;
+        font-size: 15px;
+        font-weight: 300;
+        margin-left: 12px;
+        padding: 0 11px 0 13px;
+        text-overflow: ellipsis;
+        width: 400px;
+      }
+
+      #pac-input:focus {
+        border-color: #4d90fe;
+      }
+
+      #title {
+        color: #fff;
+        background-color: #4d90fe;
+        font-size: 25px;
+        font-weight: 500;
+        padding: 6px 12px;
+      }
+      .pac-container { z-index: 100000; }
 </style>
 <div class="addproperty-onepage-form-form">
+        <div class="row">
+        <div class="pac-card" id="pac-card">
+               
+               
+                </div>
+                <div id="map"></div>
+                
+    
+        </div>
         <div class="row">
                 <?php $form = ActiveForm::begin(); ?>
             <div class="col-md-3">
@@ -36,27 +107,27 @@ label{
                     <?= $form->field($model, 'property_for')->textInput(['class' => 'form-control'])?>
                 </div>
                 <div class="col-md-3">
+                     <?= $form->field($model, 'locality')->textInput(['maxlength' => true])?>
+                </div>
+                <div class="col-md-3">
                     <?= $form->field($model, 'city')->textInput(['maxlength' => true]) ?>
                 </div>
                 
-                <div class="col-md-3">
-                 <?= $form->field($model, 'town_name')->textInput(['maxlength' => true]) ?>
-                </div>
+                
                 <div class="col-md-3">
                 <?= $form->field($model, 'sector_name')->textInput(['maxlength' => true]) ?>
                 </div>
                 <div class="col-md-3">
-                     <?= $form->field($model, 'longitude')->textInput(['class' => 'form-control'])?>
+                     <?= $form->field($model, 'latitude')->textInput(['class' => 'form-control'])?>
                 </div>
                 <div class="col-md-3">
-                     <?= $form->field($model, 'locality')->textInput(['maxlength' => true])?>
+                     <?= $form->field($model, 'longitude')->textInput(['class' => 'form-control'])?>
                 </div>
+                
                 <div class="col-md-3">
                      <?= $form->field($model, 'address')->textInput(['maxlength' => true]) ?>
                 </div>
-                <div class="col-md-3">
-                     <?= $form->field($model, 'latitude')->textInput(['class' => 'form-control'])?>
-                </div>
+                
                 <div class="col-md-3">
                      <?= $form->field($model, 'builder_name')->textInput(['maxlength' => true]) ?>
                 </div>
@@ -235,7 +306,9 @@ label{
                 <div class="col-md-3">
                 <?= $form->field($model, 'created_date')->textInput(['class' => 'form-control'])?>
                 </div>
-
+                <div class="col-md-3">
+                 <?= $form->field($model, 'town_name')->hiddenInput(['maxlength' => true])->label(false) ?>
+                </div>
             
                 <?php if (!Yii::$app->request->isAjax){ ?>
                     <div class="form-group">
@@ -246,3 +319,156 @@ label{
                 <?php ActiveForm::end(); ?>
                 
 </div>
+<script>
+      // This example requires the Places library. Include the libraries=places
+      // parameter when you first load the API. For example:
+      // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+
+      function initMap() {
+        var options = {
+           // types: ['(cities)'],
+            componentRestrictions: {country: "in"}
+            };
+          
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -33.8688, lng: 151.2195},
+          zoom: 13
+        });
+        var card = document.getElementById('pac-card');
+        var input = document.getElementById('addpropertyonepageform-locality');
+        var types = document.getElementById('type-selector');
+        var strictBounds = document.getElementById('strict-bounds-selector');
+
+        map.controls[google.maps.ControlPosition.TOP_RIGHT].push(card);
+
+        var autocomplete = new google.maps.places.Autocomplete(input,options);
+
+        // Bind the map's bounds (viewport) property to the autocomplete object,
+        // so that the autocomplete requests use the current map bounds for the
+        // bounds option in the request.
+        autocomplete.bindTo('bounds', map);
+
+        // Set the data fields to return when the user selects a place.
+        autocomplete.setFields(
+            ['address_components', 'geometry', 'icon', 'name']);
+
+        var infowindow = new google.maps.InfoWindow();
+        var infowindowContent = document.getElementById('infowindow-content');
+        infowindow.setContent(infowindowContent);
+        var marker = new google.maps.Marker({
+          map: map,
+          anchorPoint: new google.maps.Point(0, -29)
+        });
+
+        autocomplete.addListener('place_changed', function() {
+          infowindow.close();
+          marker.setVisible(false);
+          var place = autocomplete.getPlace();
+
+           var searchlat = place.geometry.location.lat();
+          var searchlng = place.geometry.location.lng();
+
+         $("#addpropertyonepageform-longitude").val(searchlng);
+$("#addpropertyonepageform-latitude").val(searchlat);
+
+
+          if (!place.geometry) {
+            // User entered the name of a Place that was not suggested and
+            // pressed the Enter key, or the Place Details request failed.
+            window.alert("No details available for input: '" + place.name + "'");
+            return;
+          }
+
+          // If the place has a geometry, then present it on a map.
+          if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+          } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);  // Why 17? Because it looks good.
+          }
+          marker.setPosition(place.geometry.location);
+          marker.setVisible(true);
+
+          var address = '';
+          if (place.address_components) {
+            address = [
+              (place.address_components[0] && place.address_components[0].short_name || ''),
+              (place.address_components[1] && place.address_components[1].short_name || ''),
+              (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+          }
+
+
+
+          var arrAddress = place.address_components;
+// console.log(places[0].address_components);
+
+$.each(arrAddress, function (i, address_component) {
+// console.log('address_component:'+i);
+
+if (address_component.types[0] == "route"){
+//console.log(i+": route:"+address_component.long_name);
+itemRoute = address_component.long_name;
+}
+
+if (address_component.types[0] == "locality"){
+// console.log("town:"+address_component.long_name); 
+itemLocality = address_component.long_name; 
+
+$('#addpropertyonepageform-city').val(itemLocality);
+$('#addpropertyonepageform-town_name').val(itemLocality);
+}
+if (address_component.types[0] == "sublocality_level_1"){
+// console.log("province:"+address_component.long_name);
+itemSectorf = address_component.long_name;
+$('#addpropertyonepageform-sector_name').val(itemSectorf);
+
+}
+
+if (address_component.types[0] == "country"){ 
+//console.log("country:"+address_component.long_name); 
+itemCountry = address_component.long_name;
+$('#country').val(itemCountry);
+}
+
+if (address_component.types[0] == "postal_code_prefix"){ 
+// console.log("pc:"+address_component.long_name); 
+itemPc = address_component.long_name;
+}
+
+if (address_component.types[0] == "street_number"){ 
+// console.log("street_number:"+address_component.long_name); 
+itemSnumber = address_component.long_name;
+}
+//return false; // break the loop 
+});
+
+          infowindowContent.children['place-icon'].src = place.icon;
+          infowindowContent.children['place-name'].textContent = place.name;
+          infowindowContent.children['place-address'].textContent = address;
+          infowindow.open(map, marker);
+        });
+
+        // Sets a listener on a radio button to change the filter type on Places
+        // Autocomplete.
+        function setupClickListener(id, types) {
+          var radioButton = document.getElementById(id);
+          radioButton.addEventListener('click', function() {
+            autocomplete.setTypes(types);
+          });
+        }
+
+        setupClickListener('changetype-all', []);
+        setupClickListener('changetype-address', ['address']);
+        setupClickListener('changetype-establishment', ['establishment']);
+        setupClickListener('changetype-geocode', ['geocode']);
+
+        document.getElementById('use-strict-bounds')
+            .addEventListener('click', function() {
+              console.log('Checkbox clicked! New state=' + this.checked);
+              autocomplete.setOptions({strictBounds: this.checked});
+            });
+      }
+    </script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDEuefpkgZlwt2EdlmUZHBVKZ4qdx6ACXA&v=3.exp&libraries=geometry,drawing,places&callback=initMap"
+        async defer></script>
--- backend/views/addproperty-onepage-form/csrindex.php
+++ backend/views/addproperty-onepage-form/csrindex.php
@@ -16,7 +16,11 @@ $this->params['breadcrumbs'][] = $this->title;
 CrudAsset::register($this);
 
 ?>
+<style>
+  
+ </style>
 <div class="addproperty-onepage-form-index">
+
     <div id="ajaxCrudDatatable">
         <?=GridView::widget([
             'id'=>'crud-datatable',
