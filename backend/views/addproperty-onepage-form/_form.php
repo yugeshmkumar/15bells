8f25106c54bc1dacd89081ee2421c5053ceae403
chirag <chirag@15bells.com>
2019-06-19 11:01:33 +0530

    backend crm

--- backend/views/addproperty-onepage-form/_form.php
+++ backend/views/addproperty-onepage-form/_form.php
@@ -29,33 +29,34 @@ label{
 <div class="addproperty-onepage-form-form">
         <div class="row">
                 <?php $form = ActiveForm::begin(); ?>
-            <div class="col-md-3">
-                    <?= $form->field($model, 'company_employee_id')->textInput(['class' => 'form-control'])?>
-            </div>
+            
             <div class="col-md-3">
                     <?= $form->field($model, 'property_for')->textInput(['class' => 'form-control'])?>
                 </div>
+                
+                <?= $form->field($model, 'completion_in_percentage')->hiddenInput()->label(false) ?>
+                
                 <div class="col-md-3">
-                    <?= $form->field($model, 'city')->textInput(['maxlength' => true]) ?>
+                    <?= $form->field($model, 'city')->textInput(['class' => 'form-control count','maxlength' => true]) ?>
                 </div>
                 
                 <div class="col-md-3">
                  <?= $form->field($model, 'town_name')->textInput(['maxlength' => true]) ?>
                 </div>
                 <div class="col-md-3">
-                <?= $form->field($model, 'sector_name')->textInput(['maxlength' => true]) ?>
+                <?= $form->field($model, 'sector_name')->textInput(['class' => 'form-control count','maxlength' => true]) ?>
                 </div>
                 <div class="col-md-3">
-                     <?= $form->field($model, 'longitude')->textInput(['class' => 'form-control'])?>
+                     <?= $form->field($model, 'longitude')->textInput(['class' => 'form-control count','class' => 'form-control'])?>
                 </div>
                 <div class="col-md-3">
-                     <?= $form->field($model, 'locality')->textInput(['maxlength' => true])?>
+                     <?= $form->field($model, 'locality')->textInput(['class' => 'form-control count','maxlength' => true])?>
                 </div>
                 <div class="col-md-3">
-                     <?= $form->field($model, 'address')->textInput(['maxlength' => true]) ?>
+                     <?= $form->field($model, 'address')->textInput(['class' => 'form-control count','maxlength' => true]) ?>
                 </div>
                 <div class="col-md-3">
-                     <?= $form->field($model, 'latitude')->textInput(['class' => 'form-control'])?>
+                     <?= $form->field($model, 'latitude')->textInput(['class' => 'form-control count','class' => 'form-control'])?>
                 </div>
                 <div class="col-md-3">
                      <?= $form->field($model, 'builder_name')->textInput(['maxlength' => true]) ?>
@@ -64,13 +65,13 @@ label{
                       <?= $form->field($model, 'project_name')->textInput(['maxlength' => true]) ?>
                 </div>
                 <div class="col-md-3">
-                    <?= $form->field($model, 'property_type_id')->textInput(['class' => 'form-control'])?>
+                    <?= $form->field($model, 'property_type_id')->textInput(['class' => 'form-control count','class' => 'form-control'])?>
                 </div>
                 <div class="col-md-3">
-                <?= $form->field($model, 'Owner_name')->textInput(['maxlength' => true]) ?>
+                <?= $form->field($model, 'Owner_name')->textInput(['class' => 'form-control count','maxlength' => true]) ?>
                 </div>
                 <div class="col-md-3">
-                <?= $form->field($model, 'primary_contact_no')->textInput(['class' => 'form-control'])?>
+                <?= $form->field($model, 'primary_contact_no')->textInput(['class' => 'form-control count','class' => 'form-control'])?>
                 </div>
                 <div class="col-md-3">
 
@@ -81,29 +82,29 @@ label{
                 <?= $form->field($model, 'landline_no')->textInput(['maxlength' => true]) ?>
                 </div>
                 <div class="col-md-3">
-                <?= $form->field($model, 'email_id')->textInput(['maxlength' => true]) ?>
+                <?= $form->field($model, 'email_id')->textInput(['class' => 'form-control count','maxlength' => true]) ?>
                 </div>
                 <div class="col-md-3">
 
                 <?= $form->field($model, 'property_on_floor')->textInput(['maxlength' => true]) ?>
                 </div>
                 <div class="col-md-3">
-                     <?= $form->field($model, 'unit_block')->textInput(['maxlength' => true]) ?>
+                     <?= $form->field($model, 'unit_block')->textInput(['class' => 'form-control count','maxlength' => true]) ?>
                 </div>
                 <div class="col-md-3">
-                        <?= $form->field($model, 'unit_number')->textInput(['maxlength' => true]) ?>
+                        <?= $form->field($model, 'unit_number')->textInput(['class' => 'form-control count','maxlength' => true]) ?>
                 </div>
                 <div class="col-md-3">
-                    <?= $form->field($model, 'buildup_area')->textInput(['class' => 'form-control'])?>
+                    <?= $form->field($model, 'super_area')->textInput(['class' => 'form-control count','class' => 'form-control'])?>
                 </div>
                 <div class="col-md-3">
-                    <?= $form->field($model, 'buildup_unit')->textInput(['maxlength' => true]) ?>
+                    <?= $form->field($model, 'super_unit')->textInput(['class' => 'form-control count','maxlength' => true]) ?>
                 </div>
                 <div class="col-md-3">
-                <?= $form->field($model, 'carpet_area')->textInput(['class' => 'form-control'])?>
+                <?= $form->field($model, 'carpet_area')->textInput(['class' => 'form-control count'])?>
                 </div>
                 <div class="col-md-3">
-                <?= $form->field($model, 'carpet_unit')->textInput(['maxlength' => true]) ?>
+                <?= $form->field($model, 'carpet_unit')->textInput(['class' => 'form-control count','maxlength' => true]) ?>
                 </div>
                 <div class="col-md-3">
 
@@ -111,7 +112,7 @@ label{
                 </div>
                 <div class="col-md-3">
 
-                <?= $form->field($model, 'total_no_of_floors')->textInput(['class' => 'form-control'])?>
+                <?= $form->field($model, 'total_no_of_floors')->textInput(['class' => 'form-control count'])?>
                 </div>
                 <div class="col-md-3">
 
@@ -123,7 +124,7 @@ label{
                 </div>
                 <div class="col-md-3">
 
-                <?= $form->field($model, 'ceiling_height')->textInput(['class' => 'form-control'])?>
+                <?= $form->field($model, 'ceiling_height')->textInput(['class' => 'form-control count'])?>
                 </div>
                 <div class="col-md-3">
 
@@ -140,7 +141,7 @@ label{
                 <?= $form->field($model, 'floor_plate_area')->textInput(['class' => 'form-control'])?>
                 </div>
                 <div class="col-md-3">
-                <?= $form->field($model, 'type_of_space')->textInput(['class' => 'form-control'])?>
+                <?= $form->field($model, 'type_of_space')->textInput(['class' => 'form-control count'])?>
                 </div>
                 <div class="col-md-3">
 
@@ -152,13 +153,13 @@ label{
                 </div>
                 <div class="col-md-3">
 
-                <?= $form->field($model, 'asking_lease_rate')->textInput(['class' => 'form-control'])?>
+                <?= $form->field($model, 'asking_lease_rate')->textInput(['class' => 'form-control count'])?>
                 </div>
                 <div class="col-md-3">
                 <?= $form->field($model, 'rate_negotiable')->textInput(['class' => 'form-control'])?>
                 </div>
                 <div class="col-md-3">
-                <?= $form->field($model, 'maintenance_charge')->textInput(['class' => 'form-control'])?>
+                <?= $form->field($model, 'maintenance_charge')->textInput(['class' => 'form-control count'])?>
                 </div>
                 <div class="col-md-3">
                 <?= $form->field($model, 'security_deposit')->textInput(['class' => 'form-control'])?>
@@ -176,7 +177,7 @@ label{
                 <?= $form->field($model, 'lease_period_restriction')->textInput(['class' => 'form-control'])?>
                 </div>
                 <div class="col-md-3">
-                <?= $form->field($model, 'max_period_lease')->textInput(['class' => 'form-control'])?>
+                <?= $form->field($model, 'max_period_lease')->textInput(['class' => 'form-control count'])?>
                 </div>
                 <div class="col-md-3">
                 <?= $form->field($model, 'open_rentfree_period')->textInput(['class' => 'form-control'])?>
@@ -185,7 +186,7 @@ label{
                 <?= $form->field($model, 'max_rentfree_period')->textInput(['class' => 'form-control'])?>
                 </div>
                 <div class="col-md-3">
-                <?= $form->field($model, 'Asking_property_price')->textInput(['class' => 'form-control'])?>
+                <?= $form->field($model, 'Asking_property_price')->textInput(['class' => 'form-control count'])?>
                 </div>
                 <div class="col-md-3">
                 <?= $form->field($model, 'price_negotiable')->textInput(['class' => 'form-control'])?>
@@ -205,36 +206,7 @@ label{
                 <div class="col-md-3">
                 <?= $form->field($model, 'property_tax_id')->textInput(['maxlength' => true]) ?>
                 </div>
-                <div class="col-md-3">
-                <?= $form->field($model, 'completion_in_percentage')->textInput(['class' => 'form-control'])?>
-                </div>
-                <div class="col-md-3">
-                <?= $form->field($model, 'property_status')->textInput(['class' => 'form-control'])?>
-                </div>
-                <div class="col-md-3">
-                <?= $form->field($model, 'property_scomment')->textInput(['class' => 'form-control'])?>
-                </div>
-                <div class="col-md-3">
-                <?= $form->field($model, 'followup_date_time')->textInput(['class' => 'form-control'])?>
-                </div>
-                <div class="col-md-3">
-                <?= $form->field($model, 'followup_comment')->textInput(['class' => 'form-control'])?>
-                </div>
-                <div class="col-md-3">
-                <?= $form->field($model, 'lead_source')->textInput(['class' => 'form-control'])?>
-                </div>
-                <div class="col-md-3">
-                <?= $form->field($model, 'site_visit')->textInput(['class' => 'form-control'])?>
-                </div>
-                <div class="col-md-3">
-                <?= $form->field($model, 'remarks')->textInput(['class' => 'form-control'])?>
-                </div>
-                <div class="col-md-3">
-                <?= $form->field($model, 'isactive')->textInput(['class' => 'form-control'])?>
-                </div>
-                <div class="col-md-3">
-                <?= $form->field($model, 'created_date')->textInput(['class' => 'form-control'])?>
-                </div>
+               
 
             
                 <?php if (!Yii::$app->request->isAjax){ ?>
@@ -246,3 +218,34 @@ label{
                 <?php ActiveForm::end(); ?>
                 
 </div>
+
+<script>
+
+$(document).ready(function(){
+
+
+    $('input').on('change', function(){
+        
+        var cntreq = 0;
+        var cntvals = 0;
+
+        $('input').each(function(i, val) {
+            if($(this).attr('class') == 'form-control count') {
+                cntreq++;
+                if($(this).val() != '') {
+                    cntvals++;
+                }
+            }
+        });
+
+        var count = (cntvals/cntreq)*100;
+        
+        $('#addpropertyonepageform-completion_in_percentage').empty();
+        $('#addpropertyonepageform-completion_in_percentage').val(Math.round(count));
+    });
+
+
+
+});
+
+</script>
