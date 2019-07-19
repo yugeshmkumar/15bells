c02a9c8f3d53d4f41946415fe0a6d9aa3b8781f7
chirag <chirag@15bells.com>
2019-07-19 15:01:25 +0530

    make link property view
    Signed-off-by: chirag <chirag@15bells.com>

--- frontend/views/lesseeaction/listings.php
+++ frontend/views/lesseeaction/listings.php
@@ -1327,10 +1327,10 @@ proptype =  $('#proptypes').val();
                             '<p class="property_id">Property ID : '+propsid+'</p>'+
                             '<div class="row single_property">'+
                                 '<div class="col-md-3 no_pad relative">'+
-                                    '<img src="<?= Yii::getAlias('@frontendUrl').'/newimg/img/blurr.jpg';  ?>" class="img-responsive">'+
-                                    '<div class="overlay_sign">'+
-                                        '<p class="sign_click"><span class="color_orange">Login</span> or <span class="color_orange">Sign</span> up to view this property</p>'+
-                                    '</div>'+
+                                '<a href="<?php echo Yii::$app->urlManager->createUrl(['addproperty/view?id=']) ?>'+this.id+'" target="_blank"> <img src="<?= Yii::getAlias('@archiveUrl').'/propertydefaultimg/';  ?>'+((this.featured_image == null) ? 'not.jpg' : imageds)+'" class="img-responsive"></a>'+
+                                    // '<div class="overlay_sign">'+
+                                    //     '<p class="sign_click"><span class="color_orange">Login</span> or <span class="color_orange">Sign</span> up to view this property</p>'+
+                                    // '</div>'+
                                 '</div>'+
                                 '<div class="col-md-9">'+
                                     '<div class="row prop_detail">'+
@@ -1491,10 +1491,10 @@ proptype =  $('#proptypes').val();
                             '<p class="property_id">Property ID : '+propsid+'</p>'+
                             '<div class="row single_property">'+
                                 '<div class="col-md-3 no_pad relative">'+
-                                    '<img src="<?= Yii::getAlias('@frontendUrl').'/newimg/img/blurr.jpg';  ?>" class="img-responsive">'+
-                                    '<div class="overlay_sign">'+
-                                        '<p class="sign_click"><span class="color_orange">Login</span> or <span class="color_orange">Sign</span> up to view this property</p>'+
-                                    '</div>'+
+                                '<a href="<?php echo Yii::$app->urlManager->createUrl(['addproperty/view?id=']) ?>'+this.id+'" target="_blank"> <img src="<?= Yii::getAlias('@archiveUrl').'/propertydefaultimg/';  ?>'+((this.featured_image == null) ? 'not.jpg' : imageds)+'" class="img-responsive"></a>'+
+                                    // '<div class="overlay_sign">'+
+                                    //     '<p class="sign_click"><span class="color_orange">Login</span> or <span class="color_orange">Sign</span> up to view this property</p>'+
+                                    // '</div>'+
                                 '</div>'+
                                 '<div class="col-md-9">'+
                                     '<div class="row prop_detail">'+
@@ -1650,10 +1650,10 @@ proptype =  $('#proptypes').val();
                             '<p class="property_id">Property ID : '+propsid+'</p>'+
                             '<div class="row single_property">'+
                                 '<div class="col-md-3 no_pad relative">'+
-                                    '<img src="<?= Yii::getAlias('@frontendUrl').'/newimg/img/blurr.jpg';  ?>" class="img-responsive">'+
-                                    '<div class="overlay_sign">'+
-                                        '<p class="sign_click"><span class="color_orange">Login</span> or <span class="color_orange">Sign</span> up to view this property</p>'+
-                                    '</div>'+
+                                '<a href="<?php echo Yii::$app->urlManager->createUrl(['addproperty/view?id=']) ?>'+this.id+'" target="_blank"> <img src="<?= Yii::getAlias('@archiveUrl').'/propertydefaultimg/';  ?>'+((this.featured_image == null) ? 'not.jpg' : imageds)+'" class="img-responsive"></a>'+
+                                    // '<div class="overlay_sign">'+
+                                    //     '<p class="sign_click"><span class="color_orange">Login</span> or <span class="color_orange">Sign</span> up to view this property</p>'+
+                                    // '</div>'+
                                 '</div>'+
                                 '<div class="col-md-9">'+
                                     '<div class="row prop_detail">'+
@@ -2509,7 +2509,9 @@ function getPolygonCoords() {
                        '<p class="property_id">Property ID : '+propsid+'</p>'+
                        '<div class="row single_property">'+
                            '<div class="col-md-3 no_pad relative">'+
-                           '<a href="<?php echo Url::to(['addproperty/view', 'id' => 1,'locality'=>'Jmd Galeria'])  ?>" target="_blank"> <img src="<?= Yii::getAlias('@archiveUrl').'/propertydefaultimg/';  ?>'+((this.featured_image == '') ? 'not.jpg' : imageds)+'" class="img-responsive"></a>'+
+                           '<a href="<?php echo Yii::$app->urlManager->createUrl(['addproperty/view?id=']) ?>'+this.id+'" target="_blank"> <img src="<?= Yii::getAlias('@archiveUrl').'/propertydefaultimg/';  ?>'+((this.featured_image == null) ? 'not.jpg' : imageds)+'" class="img-responsive"></a>'+
+
+                         //  '<a href="<?php echo Url::to(['addproperty/view', 'id' => 1,'locality'=>'Jmd Galeria'])  ?>" target="_blank"> <img src="<?= Yii::getAlias('@archiveUrl').'/propertydefaultimg/';  ?>'+((this.featured_image == '') ? 'not.jpg' : imageds)+'" class="img-responsive"></a>'+
 
                             //    '<div class="overlay_sign">'+
                             //        '<p class="sign_click"><span class="color_orange">Login</span> or <span class="color_orange">Sign</span> up to view this property</p>'+
