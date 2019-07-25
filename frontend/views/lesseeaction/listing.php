a17e270197a7e6ebb53f4b6765de0a3db8ca5b26
chirag <chirag@15bells.com>
2019-07-25 11:22:30 +0530

    listing error done

--- frontend/views/lesseeaction/listing.php
+++ frontend/views/lesseeaction/listing.php
@@ -416,7 +416,7 @@ if(!isset($_SESSION))
 
 				 ?>
 
-				 <?//= $form->errorSummary($modeled); ?>
+				
 
               <?php date_default_timezone_set("Asia/Calcutta");
               $dater = date('Y-m-d H:i:s');
