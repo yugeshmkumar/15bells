b1c72be5b0076ec6994c458995319a9ee8a78063
chirag <chirag@15bells.com>
2019-08-30 18:34:36 +0530

    rajni assign date

--- backend/controllers/AddpropertyOnepageFormController.php
+++ backend/controllers/AddpropertyOnepageFormController.php
@@ -167,9 +167,12 @@ class AddpropertyOnepageFormController extends Controller
      public function actionAssigncsrhead(){
 
         $crmid = $_POST['crmid'];
+        date_default_timezone_set("Asia/Calcutta");
+        $date = date('Y-m-d H:i:s');
         $rbac =  AddpropertyOnepageForm::find()->where(['id'=>$crmid])->one();
       
-        $rbac->isactive = 2;        
+        $rbac->isactive = 2;  
+        $rbac->reassign_date = $date;        
         $rbac->save(false);
 	if($rbac){
 	return 1;die;
