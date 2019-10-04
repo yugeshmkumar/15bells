23a3f3dd3e1916648f6e101260ca4d67bdf970bf
Chirag jhamb <chiragjhamb2905@gmail.com>
2019-10-04 17:58:18 +0530

    fb bot

--- frontend/views/layouts/homeLayout.php
+++ frontend/views/layouts/homeLayout.php
@@ -21,6 +21,32 @@ NewDesignAsset::register($this);
 <!-- <script src="https://wchat.freshchat.com/js/widget.js"></script> -->
 
 
+<!-- Load Facebook SDK for JavaScript -->
+<div id="fb-root"></div>
+      <script>
+        window.fbAsyncInit = function() {
+          FB.init({
+            xfbml            : true,
+            version          : 'v4.0'
+          });
+        };
+
+        (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));</script>
+
+      <!-- Your customer chat code -->
+      <div class="fb-customerchat"
+        attribution=setup_tool
+        page_id="100312244664763"
+  theme_color="#44bec7">
+      </div>
+
+
 		
 			<section class="navbar-info nav_bg">
 
