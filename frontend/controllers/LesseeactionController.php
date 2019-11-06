21ba16f36f967e1bdfb506444c2e5a9c570cff79
chirag <chirag@15bells.com>
2019-11-06 15:31:34 +0530

    save search

--- frontend/controllers/LesseeactionController.php
+++ frontend/controllers/LesseeactionController.php
@@ -280,12 +280,60 @@ class LesseeactionController extends Controller {
  
          date_default_timezone_set("Asia/Calcutta");
          $date = date('Y-m-d H:i:s');
+         $user_id = Yii::$app->user->identity->id;
+
+
+         if (isset(Yii::$app->user->identity->id)){
+
+            $querys = new Query;
+            $querys->select('COUNT(*) as newcount')
+                    ->from('save_searches')
+                    ->where(['user_id' => $user_id])
+                    ->andwhere(['sector' => $sector])
+                    ->andwhere(['role_type' => 'lessee']);
+
+            $commands = $querys->createCommand();
+            $paymentsm = $commands->queryOne();
+
+           // print_r($paymentsm);die;
+
+            if ($paymentsm['newcount'] == 0) {
+
+                
  
-         // if(isset(Yii::$app->user->identity->id)){
-         //     echo '1';die;
-         // }else{
-         //    echo '2';die;
-         // }  
+            
+
+           if($shaped == 'polygon'){
+              
+            $trendingadd = \Yii::$app->db->createCommand()->insert('save_searches', ['role_type' => 'lessee','search_for'=>'google', 'type' => $shaped, 'geometry' => $newspaths, 'user_id' => $user_id, 'location_name' => $locations, 'town' => $town, 'sector' => $sector, 'country' => $country, 'property_type' => $proptype, 'min_area' => $areamin, 'area' => $areamax, 'min_prices' => $pricemin, 'max_prices' => $pricemax, 'property_auction_type' => 'Instant', 'created_date' => $date])->execute();
+           }
+
+          else if($shaped == 'circle'){
+            
+            $trendingadd = \Yii::$app->db->createCommand()->insert('save_searches', ['role_type' => 'lessee','search_for'=>'google', 'type' => $shaped, 'geometry' => $centercoordinates, 'radius' => $totalradius, 'user_id' => $user_id, 'location_name' => $locations, 'town' => $town, 'sector' => $sector, 'country' => $country, 'property_type' => $proptype, 'min_area' => $areamin, 'area' => $areamax, 'min_prices' => $pricemin, 'max_prices' => $pricemax, 'property_auction_type' => 'Instant',
+                        'created_date' => $date])->execute();
+           }
+
+          else if($shaped == 'rectangle'){
+             
+            $trendingaddd = \Yii::$app->db->createCommand()->insert('save_searches', ['role_type' => 'lessee','search_for'=>'google', 'type' => $shaped, 'geometry' => $newkuma, 'radius' => $totalradius, 'user_id' => $user_id, 'location_name' => $locations, 'town' => $town, 'sector' => $sector, 'country' => $country, 'property_type' => $proptype, 'min_area' => $areamin, 'area' => $areamax, 'min_prices' => $pricemin, 'max_prices' => $pricemax, 'property_auction_type' => 'Instant',
+                        'created_date' => $date])->execute();
+                        }
+
+                        else{
+                       
+                         $trendingaddd = \Yii::$app->db->createCommand()->insert('save_searches', ['role_type' => 'lessee','search_for'=>'text', 'type' => $shaped, 'user_id' => $user_id, 'location_name' => $locations, 'town' => $town, 'sector' => $sector, 'country' => $country, 'property_type' => $proptype, 'min_area' => $areamin, 'area' => $areamax, 'min_prices' => $pricemin, 'max_prices' => $pricemax, 'property_auction_type' => 'Instant','created_date' => $date])->execute();
+
+
+                                     }
+
+      //$doshortlist = \Yii::$app->db->createCommand()->insert('shortlistproperty', ['user_id' => $user_id,'property_id'=>$propid, 'created_date' => $date, 'active' => '1'])->execute();
+                     
+       
+
+         }
+
+        }
        
          
          return $this->render('listing',[
@@ -360,6 +408,22 @@ class LesseeactionController extends Controller {
           if (isset(Yii::$app->user->identity->id)){
  
              $user_id = Yii::$app->user->identity->id;
+
+
+             $querys = new Query;
+            $querys->select('COUNT(*) as newcount')
+                    ->from('save_searches')
+                    ->where(['user_id' => $user_id])
+                    ->andwhere(['sector' => $sector])
+                    ->andwhere(['role_type' => 'lessee']);
+
+            $commands = $querys->createCommand();
+            $paymentsm = $commands->queryOne();
+
+           // print_r($paymentsm);die;
+
+            if ($paymentsm['newcount'] == 0) {
+
  
             if($shaped == 'polygon'){
                
@@ -390,6 +454,8 @@ class LesseeactionController extends Controller {
         
  
           }
+
+        }
  
         
          if($propidpost = $_SESSION['propidpostl']){
