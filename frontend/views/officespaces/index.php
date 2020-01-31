7d8cc7ab6de12edda52e5437546cfb95385bb378
Harish Rajput <doublehrajput@gmail.com>
2020-01-31 18:04:46 +0530

    cta buttons

---
+++ frontend/views/officespaces/index.php
@@ -0,0 +1,161 @@
+<?php
+
+$this->registerLinkTag(['rel' => 'canonical', 'href' => 'https://www.15bells.com/officespaces/']); 
+use yii\widgets\ActiveForm;
+
+use yii\helpers\HtmlPurifier;
+use yii\helpers\Html;
+
+
+
+?>
+
+<style>
+.alert-success{
+	position: absolute;
+    right: -7.5%;
+    top: 20%;
+    border-radius: 0;
+}
+</style>
+
+<section class="container-fluid header_bg parallax-window section" data-parallax="scroll" data-image-src="<?= Yii::getAlias('@frontendUrl').'/newimg/img/office.jpg';  ?>">
+			
+			<div class="container-fluid no_pad div_header">
+			
+				<div class="container" id="banner_cont">
+
+				<?php if (Yii::$app->session->hasFlash('success')): ?>
+  <div class="alert alert-success alert-dismissable">
+  <button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>
+  <h4><i class="icon fa fa-check"></i>Saved!</h4>
+  <?= Yii::$app->session->getFlash('success') ?>
+  </div>
+<?php endif; ?>
+
+					<div class="col-md-6 col-md-offset-1 text-left brand_desp about_bannr">
+						<h1 class="about_head">Need Office space?</h1>
+						<p class="about_det animated slideInDown">For the first time 15 Bells, a Commercial Real Estate company trades in real time. We proudly represent our self as the one-stop solution for all the commercial property needs – BUY, SELL or LEASE a commercial property in Delhi NCR Just within 15 Hours!!</p>
+						<p class="find_mor"><a class="enquire_now" href="javascript:void(0)">Find out more <img src="<?= Yii::getAlias('@frontendUrl').'/newimg/img/icons/chevron.svg';  ?>"></a></p>
+					</div>
+					
+					
+				</div>
+				
+				
+	<!-- end of navbar-->
+			</div>
+	
+		</section>
+
+
+<div class="container-fluid blog_bg">
+	<div class="row">
+		<div class="col-md-8">
+        <div class="row blog_repeat">
+                 <div class="col-md-12 no_pad">
+					 <img alt="How-RERA-helps-a-buyer" src="https://www.15bells.com/storage/web/source/1/tcvC6xHSlc5WqI-NaJQoSD2AAg_Xoexa.jpg" title="..." class="img-responsive blog_fullimage">				
+                 </div>
+				<div class="col-md-12 contnt_blg">
+					<h2 class="blog_titl">How RERA helps a buyer</h2>
+
+				
+					<p class="blog_det"><span class="writr_b">Ankur  Gupta</span>|<span class="date_b">January 1st, 2014 </span></p>
+					
+
+					<p class="blog_txt">Finally, after 10 years of long wait, RERA has finally swung into action. The government implemented RERA Act from May 1st onwards, whose main objective is to look into the grievances of the consumers.  When it comes to the real estate sector it is a known fact that customers have faced a number of issues when it comes to delivery of the under-construction properties, execution of the said plans and the legitimacy of the property.What RERA does is that it promotes accountability, transparency,</p>
+					<p class="read_m"><a href="/blogs/How-RERA-helps-a-buyer" target="_blank" class="expnd_blog">Read more</a></p>
+				</div>
+								
+			</div>
+            <div class="row blog_repeat">
+                 <div class="col-md-12 no_pad">
+                       <iframe width="100%" height="315"
+                    src="https://www.youtube.com/embed/keQPc37-1QA">
+                    </iframe>
+                 </div>
+				<div class="col-md-12 contnt_blg">
+					<h2 class="blog_titl">How RERA helps a buyer</h2>
+
+				
+					<p class="blog_det"><span class="writr_b">Ankur  Gupta</span>|<span class="date_b">January 1st, 2014 </span></p>
+					
+
+					<p class="blog_txt">Finally, after 10 years of long wait, RERA has finally swung into action. The government implemented RERA Act from May 1st onwards, whose main objective is to look into the grievances of the consumers.  When it comes to the real estate sector it is a known fact that customers have faced a number of issues when it comes to delivery of the under-construction properties, execution of the said plans and the legitimacy of the property.What RERA does is that it promotes accountability, transparency,</p>
+					<p class="read_m"><a href="/blogs/How-RERA-helps-a-buyer" target="_blank" class="expnd_blog">Read more</a></p>
+				</div>
+								
+			</div>
+		</div>
+	<!---Blog Side menu----->
+		<div class="col-md-4 resource_form">
+        
+        <a href="tel:6209151515"><p class="call_no text-center"><img src="<?= Yii::getAlias('@frontendUrl').'/newimg/phone.svg';  ?>" width="17"> +91-935573-15-15</p></a>
+           <?php $form = ActiveForm::begin(['id' => $model->formName(),'action'=>"coworking"]); ?>
+           <h3 class="side_head">Contact Us</h3>
+                    <div class="row">
+                        <div class="col-md-12 no_pad border_form">
+                            <div class="row padd_contact">
+                                <div class="col-md-6">
+                                    <?php echo $form->field($model, 'name')->textInput(['maxlength' => true, 'placeholder' => "Full Name", 'class'=>'form-control input_desgn'])->label(false); ?>
+
+                                </div>
+                                <div class="col-md-6">
+                                    <?php echo $form->field($model, 'phone')->textInput(['minlength' => 10,'maxlength' => 10,'maxlength' => true, 'placeholder' => "Phone No", 'class'=>'form-control input_desgn'])->label(false); ?>
+
+                                </div>
+                                
+                            </div>
+                            <div class="row padd_contact">
+                                <div class="col-md-6">
+                                    <?php echo $form->field($model, 'email')->textInput(['maxlength' => true, 'placeholder' => "Email", 'class'=>'form-control input_desgn'])->label(false); ?>
+
+                                </div>
+                                <div class="col-md-6">
+                                    <?php echo $form->field($model, 'seats')->textInput(['maxlength' => true, 'placeholder' => "Total Area", 'class'=>'form-control input_desgn'])->label(false); ?>
+
+                                </div>
+                            </div>
+                            <div class="row padd_contact">
+                                <div class="col-md-12">
+                                    <?php echo $form->field($model, 'message')->textArea(['maxlength' => true, 'placeholder' => "Message", 'class'=>'form-control input_desgn'])->label(false); ?>
+
+                                </div>
+                            </div>
+                            <div class="row padd_contact">
+                                <div class="col-md-12">
+                                <?= Html::submitButton('Send Message', ['class' => 'send_messgae sign_up']) ?>
+
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    
+                
+            <?php ActiveForm::end(); ?>
+		</div>
+	</div>
+</div>
+	
+			
+
+
+
+
+
+<?php 
+$script = <<< JS
+
+setTimeout(function() { $('.alert-success').fadeOut('fast'); }, 8000);
+
+     $(".enquire_now").click(function () {
+      $('html,body').animate({
+        scrollTop: $(".contact_enquiry").offset().top - 100},
+        'slow');   
+		});
+		
+
+JS;
+$this->registerJs($script);
+?>  
