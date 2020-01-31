0feb5ea14815fe55295f2768327e212b45b995c5
chirag <chirag@15bells.com>
2020-01-31 17:50:19 +0530

    inspectlet

--- frontend/views/layouts/homeLayout_new.php
+++ frontend/views/layouts/homeLayout_new.php
@@ -16,6 +16,32 @@ NewDesignAsset::register($this);
 ?>
 <?php $this->beginContent('@frontend/views/layouts/base10signup.php'); ?>
 
+!-- <script src="https://wchat.freshchat.com/js/widget.js"></script> -->
+<!-- Load Facebook SDK for JavaScript -->
+<div id="fb-root"></div>
+<script>
+window.fbAsyncInit = function() {
+  FB.init({
+    xfbml            : true,
+    version          : 'v5.0'
+  });
+};
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<!-- Your customer chat code -->
+<div class="fb-customerchat"
+  attribution=install_email
+  page_id="251892915527127"
+  theme_color="#c4984f">
+</div>
+
 
 
 <body>
