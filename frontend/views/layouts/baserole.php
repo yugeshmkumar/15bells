7c788e8f2028b6e753caff155b403151a04da403
chirag <chirag@15bells.com>
2020-02-14 15:05:51 +0530

    base

--- frontend/views/layouts/baserole.php
+++ frontend/views/layouts/baserole.php
@@ -40,7 +40,14 @@ $this->params['body-class'] = array_key_exists('body-class', $this->params) ?
   gtag('config', 'UA-153152050-1');
 </script>
 
-
+<script type='text/javascript'>
+  window.smartlook||(function(d) {
+    var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+    var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
+    c.charset='utf-8';c.src='https://rec.smartlook.com/recorder.js';h.appendChild(c);
+    })(document);
+    smartlook('init', '3ecf0527cc2bf98dd37e0cde5d58f51e1ebd1a61');
+</script>
 
 
 </head>
