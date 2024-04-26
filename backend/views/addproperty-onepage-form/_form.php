9aed468c2aec139ed296c8117b5a62aa2a7933d0
Yo-FirsThing <yogesh@firsthing.in>
2024-04-27 00:57:36 +0530

    changes

--- backend/views/addproperty-onepage-form/_form.php
+++ backend/views/addproperty-onepage-form/_form.php
@@ -366,8 +366,7 @@ label{
             
                 <?php if (!Yii::$app->request->isAjax){ ?>
                     <div class="form-group">
-                        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Approve', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
-                        <?= Html::submitButton( 'Update', ['class' => 'btn btn-primary']) ?>
+                        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
                     </div>
                 <?php } ?>
 
