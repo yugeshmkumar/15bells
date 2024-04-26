afd0ab70c158cc32eafbb8a71bf4efdcef8ee8a5
Yo-FirsThing <yogesh@firsthing.in>
2024-04-27 00:50:47 +0530

    changes to fix button functionality

--- backend/views/addproperty-onepage-form/_csrheadcolumns.php
+++ backend/views/addproperty-onepage-form/_csrheadcolumns.php
@@ -150,19 +150,23 @@ return [
     [
         'class' => 'kartik\grid\ActionColumn',
         'dropdown' => false,
-        'vAlign'=>'middle',
+        'vAlign' => 'middle',
         'options' => ['style' => 'width:100px;'],
-        'urlCreator' => function($action, $model, $key, $index) { 
-                return Url::to([$action,'id'=>$key]);
+        'urlCreator' => function ($action, $model, $key, $index) {
+            return Url::to([$action, 'id' => $key]);
         },
-        'viewOptions'=>['role'=>'modal-remote','title'=>'View','data-toggle'=>'tooltip'],
-        'updateOptions'=>['role'=>'modal-remote','title'=>'Update', 'data-toggle'=>'tooltip'],
-        'deleteOptions'=>['role'=>'modal-remote','title'=>'Delete', 
-                          'data-confirm'=>false, 'data-method'=>false,// for overide yii data api
-                          'data-request-method'=>'post',
-                          'data-toggle'=>'tooltip',
-                          'data-confirm-title'=>'Are you sure?',
-                          'data-confirm-message'=>'Are you sure want to delete this item'], 
+        'viewOptions' => ['role' => 'button', 'title' => 'View', 'data-toggle' => 'tooltip'],
+        'updateOptions' => ['role' => 'button', 'title' => 'Update', 'data-toggle' => 'tooltip'],
+        'deleteOptions' => [
+            'role' => 'modal-remote',
+            'title' => 'Delete',
+            'data-confirm' => false,
+            'data-method' => false,
+            'data-request-method' => 'post',
+            'data-toggle' => 'tooltip',
+            'data-confirm-title' => 'Are you sure?',
+            'data-confirm-message' => 'Are you sure want to delete this item'
+        ],
     ],
 ];
 
